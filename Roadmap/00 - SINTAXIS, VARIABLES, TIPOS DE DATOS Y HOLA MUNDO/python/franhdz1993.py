0a636fbd19f41e927b8bc0bf42e2909f47f8bf63
Fran_prog_2024 <franhdzg1993@gmail.com>
2024-07-10 14:04:32 -0600

    franhdz1993.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/franhdz1993.py
@@ -0,0 +1,31 @@
+# https://python.com
+
+# esto es un comentario en una linea
+""" esto tambien es un comentario
+pero en varias lineas"""
+
+'''esto es otro comentario 
+en varias lineas'''
+
+# Variable
+
+my_variable= "Mi variable"
+
+"""CONSTANTE
+"""
+MY_CONSTANT= "mi otra variable" # una constante es una variable en mayusculas, pero se pone en mayusculas para quien conoce de python sepa que no se debe cambiar
+
+#datos primitivos
+
+my_int = 1
+my_float = 1.5
+my_boolean = True
+my_boolean = False
+my_string ="mi cadena de texto"
+
+print("¡Hola Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_boolean))
+print(type(my_string))
